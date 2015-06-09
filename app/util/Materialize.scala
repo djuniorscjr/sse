ae85c9684c5089fe9fcc077a7af877ec515e76b1
Domingos Junior <djunior_s@hotmail.com>
2015-06-09 13:12:12 -0300

    implementando construtor de inputs

---
+++ app/util/Materialize.scala
@@ -0,0 +1,17 @@
+package util
+
+import play.twirl.api.Html
+import views.html.helper.{FieldElements, FieldConstructor}
+import views.html.materializecss.{passwordInput, textInput}
+
+/**
+ * Created by Domingos Junior on 09/06/2015.
+ */
+object Materialize {
+        implicit val text = new FieldConstructor {
+          override def apply(elts: FieldElements): Html = textInput(elts)
+        }
+        implicit val password = new FieldConstructor {
+          override def apply(elts: FieldElements): Html = passwordInput(elts)
+        }
+}
